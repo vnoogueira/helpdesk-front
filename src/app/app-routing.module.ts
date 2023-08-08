6957ea22b3864d3384448bf0576684118345d927
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-08-08 00:28:21 -0300

    Route to component home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: NavComponent
-  }
+    component: NavComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 
 @Component({
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  constructor(private router: Router){
+  }
+
+  ngOnInit(): void {
+    this.router.navigate(['home'])
+  }
 
 }
