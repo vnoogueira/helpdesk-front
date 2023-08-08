21f6762154d8fe2cd870af0bbae65a38f01ac8b2
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-08-08 00:19:42 -0300

    Created component home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,12 +29,14 @@ import { MatCardModule } from '@angular/material/card';
 
 //Import Components of project
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
