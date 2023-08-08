29eaa8e1af25fc22f7555967e6f7d94363365681
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-08-07 22:53:28 -0300

    Component Nav created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,10 +27,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
+//Import Components of project
+import { NavComponent } from './components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
