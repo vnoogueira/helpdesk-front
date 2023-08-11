9a178e2ce61709e7fef6529d58bb53cff26954b7
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-08-11 12:39:26 -0300

    Route to component tecnico-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   {
@@ -23,6 +24,10 @@ const routes: Routes = [
       {
         path: 'tecnicos',
         component: TecnicoListComponent
+      },
+      {
+        path: 'tecnicos/create',
+        component: TecnicoCreateComponent
       }
     ]
   },
