76d189ade9ecfaf94d8c0be38f1cde27d8d5db70
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-08-15 00:02:28 -0300

    add button edit and delete

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.router.navigate(['tecnicos/create'])
+    this.router.navigate(['tecnicos'])
   }
 
   logout(){
