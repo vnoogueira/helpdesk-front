2aa884734e97e5bcc7c7d8b5d8cfaa33f8561601
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-08-18 12:43:08 -0300

    Created component chamado-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 
 @NgModule({
@@ -63,7 +64,8 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
     ClienteListComponent,
     ClienteUpdateComponent,
     ClienteCreateComponent,
-    ClienteDeleteComponent
+    ClienteDeleteComponent,
+    ChamadoListComponent
   ],
   imports: [
     BrowserModule,
