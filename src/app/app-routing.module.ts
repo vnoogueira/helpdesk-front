44a3265e2db0db0b7f05f4955b8ed35168a8b2ad
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-08-21 18:26:39 -0300

    Created route chamado-create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 const routes: Routes = [
   {
@@ -63,6 +64,10 @@ const routes: Routes = [
       {
         path: 'chamados',
         component: ChamadoListComponent
+      },
+      {
+        path: 'chamados/create',
+        component: ChamadoCreateComponent
       }
     ]
   },
