03302b69d8a3ac33b6a381039ce1a0d0f2c7d79c
Vitor Nogueira <vitor.nogueira94@gmail.com>
2023-08-24 13:04:34 -0300

    Ajustes no css

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { AuthService } from 'src/app/services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { timeout } from 'rxjs';
@@ -15,17 +15,30 @@ export class NavComponent implements OnInit {
   constructor(
     private router: Router,
     private authService: AuthService,
-    private toast: ToastrService){
+    private toast: ToastrService,
+  ) {
   }
 
   ngOnInit(): void {
     this.router.navigate(['chamados'])
   }
 
-  logout(){
+  logout() {
     this.router.navigate(['login'])
     this.authService.logout();
-    this.toast.info('Logout realizado com sucesso !', 'Logout', {timeOut:7000})
+    this.toast.info('Logout realizado com sucesso !', 'Logout', { timeOut: 7000 })
+  }
+
+  changeIcons() {
+    if (document.getElementById('mat-icon').innerText = "menu") {
+      document.getElementById('mat-icon').innerText = "close";
+    }
+  }
+
+  changeIcon(){
+    if (document.getElementById('mat-icon').innerText = "close") {
+      document.getElementById('mat-icon').innerText = "menu";
+    } 
   }
 
 }
